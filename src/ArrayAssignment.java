6f18354cdf9b3dad0702f5a74152ff8818b5431e
eliasubz <eliasls2002@yahoo.de>
2022-10-11 00:26:09 +0200

    line was printed

--- src/ArrayAssignment.java
+++ src/ArrayAssignment.java
@@ -1,9 +1,12 @@
 public class ArrayAssignment {
 
     public static void main(String[] args) {
-
+        for(int i = 0; i < 1; i++){
+            int a = i+i;
+        }
     }
     public static int allNumsWithin(int[] A, int k) {
+        System.out.println("This was sent from laptop");
         return 0;
     }
 
