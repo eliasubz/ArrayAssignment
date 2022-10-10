fd408fd636f484dec06fcafb78a56665baa5cee8
PC <eliasls2002@yahoo.de>
2022-10-11 00:04:05 +0200

    Initialize the project

---
+++ src/ArrayAssignment.java
@@ -0,0 +1,13 @@
+public class ArrayAssignment {
+
+    public static void main(String[] args) {
+
+    }
+    public static int allNumsWithin(int[] A, int k) {
+        return 0;
+    }
+
+    public static void diagonalPrint(int[][] M) {
+
+    }
+}
